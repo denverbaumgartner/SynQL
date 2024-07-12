5a4264fd9d4adb4c056e191cf270cd033b0f23a8
denver <denver@semiotic.ai>
2024-07-12 17:25:41 -0400

    test: create statements

---
+++ synql/tests/test_main.py
@@ -0,0 +1,16 @@
+import pytest 
+from synsql import SynSql
+
+class TestMain:
+
+    def test_get_spider_create_statements(self, syn: SynSql):
+        create_statements = syn.get_spider_create_statements()
+        assert create_statements is not None
+        print(create_statements['perpetrator'])
+        assert isinstance(create_statements['perpetrator'], str)
+        
+        # perpetrator_schema = """CREATE TABLE perpetrator (perpetrator_id NUMBER, people_id NUMBER, date TEXT, YEAR NUMBER, LOCATION TEXT, country TEXT, killed NUMBER, injured NUMBER,
+        #                         FOREIGN KEY (people_id) REFERENCES people(people_id));
+        #                         CREATE TABLE people (people_id NUMBER, name TEXT, height NUMBER, weight NUMBER, home_town TEXT);"""
+
+        # assert create_statements['perpetrator'] == perpetrator_schema
